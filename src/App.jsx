e84beb0cb50b583d8884d92ad56462bab36e936d
Mariano Orozco <111398222+Mariano2001O@users.noreply.github.com>
2023-11-01 11:10:52 -0300

    pullingg

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { BrowserRouter as BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import Login from './pages/Login';
-import { PerfilUser } from './pages/PerfilUser';
-import Register from './pages/Register';
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import NavBar from "./components/NavBar"
+
 
 export const App = () => {
   return (
     <>
-    <NavBar></NavBar>
+    <NavBar/>
     </>
   )};
- 
+
 export default App
