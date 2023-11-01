438a8bd5460c4cdbeebfb177f6211a04a0a4d30c
facuforipa01 <facufolatelli2001@gmail.com>
2023-11-01 09:26:11 -0300

    volviendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Login from './pages/Login';
