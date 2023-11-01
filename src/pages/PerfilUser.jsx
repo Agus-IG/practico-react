547e27969f6e31078217f86e420e7cba6f671833
AgusIG <agusig026@gmail.com>
2023-11-01 14:06:59 -0300

    añadiendo avatar

--- src/pages/PerfilUser.jsx
+++ src/pages/PerfilUser.jsx
@@ -1,2 +1,13 @@
-export function PerfilUser(){
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+export function PerfilUser() {
+    return (
+        <>
+            <div>
+                <h1>Hola</h1>
+                <Avatar alt="Remy Sharp" src="/public/foto-cv.jpg" />
+            </div>
+        </>
+    )
 }
