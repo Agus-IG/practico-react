2971f5247bdf1c29710a091082d048b33a84d244
Mariano Orozco <111398222+Mariano2001O@users.noreply.github.com>
2023-11-01 10:56:59 -0300

    pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { BrowserRouter as BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import { Home } from './pages/Home';
 import Login from './pages/Login';
 import { PerfilUser } from './pages/PerfilUser';
 import Register from './pages/Register';
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
