09708c115cfefb87a582fc97a512b5d0f0282936
facuforipa01 <facufolatelli2001@gmail.com>
2023-11-01 11:01:27 -0300

    navBar andando, con estilos de Diego

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { BrowserRouter as BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Home } from './pages/Home';
-import Login from './pages/Login';
-import { PerfilUser } from './pages/PerfilUser';
-import Register from './pages/Register';
 import * as React from 'react';
-
 import NavBar from './components/navBar';
 
 export const App = () => {
