86cf2b4b4132b95c54344f0fe73312147cf24956
Loletame <loletame0100@gmail.com>
2023-10-31 21:30:01 -0300

    update login falta logica

---
+++ src/indeximg.jsx
@@ -0,0 +1,13 @@
+
+import image1 from './assets/1.jpeg';
+import image2 from './assets/2.jpg';
+import image3 from './assets/3.jpg';
+
+const imageFiles = {
+    image1,
+    image2,
+    image3,
+    
+  };
+  
+  export default imageFiles;
