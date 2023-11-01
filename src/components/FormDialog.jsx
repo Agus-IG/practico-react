91e4827b6fb5a468ea31fe52c6d69c374f26717a
AgusIG <agusig026@gmail.com>
2023-11-01 15:53:14 -0300

    estructura, inputs, form y estilo

---
+++ src/components/FormDialog.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function FormDialog(props) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            <Button variant="contained" onClick={handleClickOpen}>
+                {props.title}
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>{props.title}</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        id="standard-basic"
+                        label="Usuario"
+                        defaultValue="Agustin Gonzalez"
+                        variant="filled"
+                    />
+                    <TextField
+                        id="standard-password-input"
+                        label="Password"
+                        type="password"
+                        defaultValue="Agustin Gonzalez"
+                        autoComplete="current-password"
+                        variant="filled"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancelar</Button>
+                    <Button onClick={handleClose}>Aceptar</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
--- src/pages/PerfilUser.jsx
+++ src/pages/PerfilUser.jsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import { estilos } from '../Styles';
+import FormDialog from '../components/FormDialog';
+import TextField from '@mui/material/TextField';
 
 export function PerfilUser() {
     return (
         <>
-            <div>
-                <h1>Hola</h1>
-                <Avatar alt="Remy Sharp" src="/public/foto-cv.jpg" />
+            <div style={estilos.profile}>
+                <h1 style={estilos.titulos}>Perfil</h1>
+                <Avatar alt="Remy Sharp" src="/public/foto-cv.jpg" style={estilos.avatar} />
+                <TextField
+                    disabled
+                    label="Usuario"
+                    id="outlined-size-small"
+                    defaultValue="Agustin Gonzalez"
+                    size="small"
+                    variant="filled"
+                />
+                <TextField
+                    disabled
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    defaultValue="Agustin Gonzalez"
+                    autoComplete="current-password"
+                    variant="filled"
+                />
+                <FormDialog title="Editar perfil" />
             </div>
         </>
     )
