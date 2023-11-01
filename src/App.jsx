64a0c09ac6eaff0c42ca18184643dd61776e7eb6
facuforipa01 <facufolatelli2001@gmail.com>
2023-11-01 10:45:04 -0300

    navBar andando, con estilos de Diego

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,17 @@
-import { BrowserRouter as BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Home } from './pages/Home';
 import Login from './pages/Login';
 import { PerfilUser } from './pages/PerfilUser';
 import Register from './pages/Register';
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+
+import NavBar from './components/navBar';
 
 export const App = () => {
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          
-          <Button color="inherit" >Login</Button>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Perfil</Button>
-
-        </Toolbar>
-      </AppBar>
-    </Box>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path='/home' Component={Home}>
-          </Route>
-          <Route path='/login' Component={Login}>
-          </Route>
-          <Route path='/perfiluser' Component={PerfilUser}>
-          </Route>
-          <Route path='/register' Component={Register}>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-      <a href="/home">Inicio</a>
-      <a href="/login">login</a>
-      <a href="/perfiluser">perfil del usuario</a>
-      <a href="/register">registro</a>
+    <NavBar></NavBar>
     </>
   )};
-
-
+ 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import { BrowserRouter as BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import { Home } from '../pages/Home';
+import Login from '../pages/Login';
+import { PerfilUser } from '../pages/PerfilUser';
+import Register from '../pages/Register';
+import { estilos } from '../Styles';
+
+export const NavBar = () => {
+  return (
+    <>
+    <BrowserRouter>
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={estilos.navBar} >
+        <Toolbar>
+          
+
+          <Link to={'./'}><Button color="inherit" style={estilos.boton2}>Home</Button></Link>
+          <Link to={'./login'}><Button color="inherit" style={estilos.boton2}>Login</Button></Link>
+          <Link to={'./perfiluser'}><Button color="inherit" style={estilos.boton2}>Perfil</Button></Link>
+          <Link to={'./register'}><Button color="inherit" style={estilos.boton2}>Registrarse</Button></Link>
+
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+    <Routes>
+          <Route exact path='/' Component={Home}>
+          </Route>
+          <Route path='/login' Component={Login}>
+          </Route>
+          <Route path='/perfiluser' Component={PerfilUser}>
+          </Route>
+          <Route path='/register' Component={Register}>
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+
+      
+    </>
+  )};
+
+    
+
+export default NavBar
